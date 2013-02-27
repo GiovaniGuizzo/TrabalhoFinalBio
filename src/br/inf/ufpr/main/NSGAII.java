12dc9b40a47b7cd79e040af3a98e1b54750764e1
giovaniguizzo <giovaniguizzo@192.168.1.100>
2013-02-27 11:10:20 -0300

    Hypervolume funcionando agora.

--- src/br/inf/ufpr/main/NSGAII.java
+++ src/br/inf/ufpr/main/NSGAII.java
@@ -12,12 +12,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import jmetal.core.Algorithm;
 import jmetal.core.Operator;
-import jmetal.core.Problem;
 import jmetal.core.SolutionSet;
 import jmetal.operators.crossover.*;
 import jmetal.operators.mutation.*;
 import jmetal.operators.selection.*;
 import jmetal.qualityIndicator.Hypervolume;
+import jmetal.qualityIndicator.QualityIndicator;
 import jmetal.util.JMException;
 
 /**
@@ -51,22 +51,22 @@ public class NSGAII {
             SecurityException,
             IOException,
             ClassNotFoundException {
-        
-        if (args.length < 4) {
-            System.out.println("You must inform the following arguments:");
-            System.out.println("\t1 - Population Size (int);");
-            System.out.println("\t2 - Max Evaluations (int);");
-            System.out.println("\t3 - Crossover Probability (double);");
-            System.out.println("\t4 - Mutation Probability (double);");
-            System.exit(0);
-        } else {
-            POPULATION_SIZE = Integer.valueOf(args[0]);
-            MAX_EVALUATION = Integer.valueOf(args[1]);
-            CROSSOVER_PROBABILITY = Double.valueOf(args[2]);
-            MUTATION_PROBABILITY = Double.valueOf(args[3]);
-        }
 
-        Problem problem; // The problem to solve
+//        if (args.length < 4) {
+//            System.out.println("You must inform the following arguments:");
+//            System.out.println("\t1 - Population Size (int);");
+//            System.out.println("\t2 - Max Evaluations (int);");
+//            System.out.println("\t3 - Crossover Probability (double);");
+//            System.out.println("\t4 - Mutation Probability (double);");
+//            System.exit(0);
+//        } else {
+//            POPULATION_SIZE = Integer.valueOf(args[0]);
+//            MAX_EVALUATION = Integer.valueOf(args[1]);
+//            CROSSOVER_PROBABILITY = Double.valueOf(args[2]);
+//            MUTATION_PROBABILITY = Double.valueOf(args[3]);
+//        }
+
+        TestCaseMinimizationProblem problem; // The problem to solve
         Algorithm algorithm; // The algorithm to use
         Operator crossover; // Crossover operator
         Operator mutation; // Mutation operator
@@ -96,7 +96,7 @@ public class NSGAII {
         parameters = new HashMap();
         parameters.put("problem", problem);
         parameters.put("populationSize", algorithm.getInputParameter("populationSize"));
-        selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters);
+        selection = SelectionFactory.getSelectionOperator("BinaryTournament", parameters);
 
         // Add the operators to the algorithm
         algorithm.addOperator("crossover", crossover);
@@ -105,14 +105,15 @@ public class NSGAII {
 
         double[] hypervolume = new double[EXECUCOES];
 
-        Hypervolume qualityIndicator = new Hypervolume();
-
         long initTime = System.currentTimeMillis();
 
         File dir = new File("RESULT_" + initTime);
         if (!dir.exists()) {
             dir.mkdir();
         }
+
+        problem.writeHypervolumeParetoFront(dir.getPath() + "/PARETO");
+        QualityIndicator indicator = new QualityIndicator(problem, dir.getPath() + "/PARETO");
 
         for (int i = 0; i < EXECUCOES; i++) {
             // Execute the Algorithm
@@ -121,79 +122,15 @@ public class NSGAII {
 
             // Result messages 
             population.sortSolutions();
-            population.convertObjective(1);
-            population.printVariablesToFile("RESULT_" + initTime + "/VAR_" + i + ".dat");
-            population.printObjectivesToFile("RESULT_" + initTime + "/FUN_" + i + ".dat");
+            population.printVariablesToFile(dir.getPath() + "/VAR_" + i + ".dat");
+            population.printObjectivesToFile(dir.getPath() + "/FUN_" + i + ".dat");
 
             //Hypervolume
-            double[][] solutionFront = qualityIndicator.utils_.readFront("RESULT_" + initTime + "/FUN_" + i + ".dat");
-
-            //Obtain delta value
-            double value = qualityIndicator.calculateHypervolume(solutionFront, solutionFront.length, 2);
-
-//            System.out.println("Execution " + i + " done! Total execution time: " + estimatedTime / 1000 + "s");
-//            System.out.println("Estimated time remain to completion: " + ((29 - i) * (estimatedTime / 1000)) + "s");
-//            System.out.println("Hypervolume " + i + ": " + value);
+            double value = indicator.getHypervolume(population);
             hypervolume[i] = value;
+            System.out.println(value);
         }
         long estimatedTime = System.currentTimeMillis() - initTime;
-        writeHypervolume("RESULT_" + initTime + "/A_RESULT", EXECUCOES, hypervolume, estimatedTime);
+        problem.writeHypervolume(dir.getPath() + "/A_RESULT", EXECUCOES, POPULATION_SIZE, MAX_EVALUATION, MUTATION_PROBABILITY, CROSSOVER_PROBABILITY, hypervolume, estimatedTime);
     } //main
-
-    public static void writeHypervolume(String filePath, int execucoes, double[] hypervolume, long estimatedTime) {
-        FileOutputStream fos = null;
-        try {
-            File file = new File(filePath);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            fos = new FileOutputStream(file);
-            OutputStreamWriter osw = new OutputStreamWriter(fos);
-            BufferedWriter bw = new BufferedWriter(osw);
-
-            double lowerHypervolume = Double.MAX_VALUE;
-            int bestFile = 0;
-            double mean = 0;
-            for (int i = 0; i < execucoes; i++) {
-                mean += hypervolume[i];
-                if (hypervolume[i] < lowerHypervolume) {
-                    lowerHypervolume = hypervolume[i];
-                    bestFile = i;
-                }
-            }
-            mean = mean / execucoes;
-
-            bw.write("Crossover Probability: " + CROSSOVER_PROBABILITY);
-            bw.newLine();
-            bw.write("Mutation Probability: " + MUTATION_PROBABILITY);
-            bw.newLine();
-            bw.write("Population Size: " + POPULATION_SIZE);
-            bw.newLine();
-            bw.write("Max Evaluations: " + MAX_EVALUATION);
-            bw.newLine();
-            bw.write("Number of Generations: " + (MAX_EVALUATION / POPULATION_SIZE));
-            bw.newLine();
-            bw.write("Hypervolume Mean: " + mean);
-            bw.newLine();
-            bw.write("Best Pareto: Execution " + bestFile);
-            bw.newLine();
-            bw.write("Best Hypervolume: " + lowerHypervolume);
-            bw.newLine();
-            bw.newLine();
-            bw.write("Execution Time: " + estimatedTime / 100);
-
-            bw.flush();
-            bw.close();
-        } catch (IOException ex) {
-            Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                if (fos != null) {
-                    fos.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
 } // NSGAII_main
--- src/br/inf/ufpr/representation/problem/TestCaseMinimizationProblem.java
+++ src/br/inf/ufpr/representation/problem/TestCaseMinimizationProblem.java
@@ -4,13 +4,21 @@
  */
 package br.inf.ufpr.representation.problem;
 
+import br.inf.ufpr.main.NSGAII;
 import br.inf.ufpr.representation.solution.ProductArraySolutionType;
 import br.inf.ufpr.pojo.Mutant;
 import br.inf.ufpr.pojo.Product;
 import br.inf.ufpr.pojo.ProductMutant;
 import br.inf.ufpr.representation.variable.ProductVariable;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import jmetal.core.Problem;
 import jmetal.core.Solution;
 import jmetal.util.JMException;
@@ -56,8 +64,95 @@ public class TestCaseMinimizationProblem extends Problem {
         }
 
         solution.setObjective(0, decisionVariables.length);
-        solution.setObjective(1, hash.size() * -1);
-        
+        solution.setObjective(1, mutants.size() - hash.size());
+
 //        System.out.println("Fitness: " + solution.getObjective(0) + " / " + solution.getObjective(1));
+    }
+
+    public void writeHypervolumeParetoFront(String filePath) {
+        FileOutputStream fos = null;
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fos = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            bw.write(this.getProducts().size() + " 0.0");
+            bw.newLine();
+            bw.write("0.0 " + this.getMutants().size());
+
+            bw.flush();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void writeHypervolume(String filePath, int execucoes, int populationSize, int maxEvaluations, double mutationProbability, double crossoverProbability, double[] hypervolume, long estimatedTime) {
+        FileOutputStream fos = null;
+        try {
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            fos = new FileOutputStream(file);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            double lowerHypervolume = Double.MAX_VALUE;
+            int bestFile = 0;
+            double mean = 0;
+            for (int i = 0; i < execucoes; i++) {
+                mean += hypervolume[i];
+                if (hypervolume[i] < lowerHypervolume) {
+                    lowerHypervolume = hypervolume[i];
+                    bestFile = i;
+                }
+            }
+            mean = mean / execucoes;
+
+            bw.write("Crossover Probability: " + crossoverProbability);
+            bw.newLine();
+            bw.write("Mutation Probability: " + mutationProbability);
+            bw.newLine();
+            bw.write("Population Size: " + populationSize);
+            bw.newLine();
+            bw.write("Max Evaluations: " + maxEvaluations);
+            bw.newLine();
+            bw.write("Number of Generations: " + (maxEvaluations / populationSize));
+            bw.newLine();
+            bw.write("Hypervolume Mean: " + mean);
+            bw.newLine();
+            bw.write("Best Pareto: Execution " + bestFile);
+            bw.newLine();
+            bw.write("Best Hypervolume: " + lowerHypervolume);
+            bw.newLine();
+            bw.newLine();
+            bw.write("Execution Time: " + estimatedTime / 100);
+
+            bw.flush();
+            bw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(NSGAII.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
